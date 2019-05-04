e8dccdad2afddf52efebfb8063bab512275e4681
Hessam A Shokravi <ahs502@gmail.com>
2019-05-04 15:57:17 +0430

    - Material-UI is added  - CSSBaseLine is added  - ThemeProvider is added

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'typeface-roboto';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#596BEC' },
+    secondary: purple
+  }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <React.Fragment>
+      <CssBaseline />
+      <App />
+    </React.Fragment>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

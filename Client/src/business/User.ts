dd6da218cbd11b8516da20ac761ed88d6d015add
951168 <afshin.alizadeh@mahan.aero>
2019-05-18 08:50:14 +0430

    - AppBar ui complited - Change AppBar position to relative (for Show shadow)

---
+++ Client/src/business/User.ts
@@ -0,0 +1,3 @@
+export default interface User {
+  displayName: string;
+}
--- Client/src/components/AppBar.tsx
+++ Client/src/components/AppBar.tsx
@@ -1,26 +1,77 @@
 import React, { PureComponent } from 'react';
 import { WithStyles, createStyles, withStyles, Theme } from '@material-ui/core/styles';
+import { AppBar as MaterialUiAppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { ArrowBackIos as ArrowBackIcon, Sync as SyncIcon, PersonalVideo as ViewModuleIcon, Fullscreen as FullScreenIcon, FullscreenExit as ExitFullScreenIcon } from '@material-ui/icons';
+import User from '../business/User';
+import classNames from 'classnames';
 
 const styles = (theme: Theme) =>
   createStyles({
-    root: {
-      border: '1px solid ' + theme.palette.primary.dark,
-      backgroundColor: theme.palette.primary.main,
-      margin: 0,
-      padding: theme.spacing.unit * 2
+    textMargin: {
+      marginLeft: theme.spacing.unit * 1.5,
+      marginRight: theme.spacing.unit * 1.5
+    },
+    grow: {
+      flexGrow: 1
+    },
+    iconSize: {
+      fontSize: 40
+    },
+    backIcon: {
+      position: 'relative',
+      left: 5
     }
   });
 
-interface Props extends WithStyles<typeof styles> {}
+interface Props extends WithStyles<typeof styles> {
+  user: User;
+}
 
-class AppBar extends PureComponent<Props> {
+interface State {
+  isFullScreen: boolean;
+}
+
+class AppBar extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isFullScreen: false
+    };
+  }
+
+  toggleFullScreen = () => {
+    this.setState({ ...this.state, isFullScreen: !this.state.isFullScreen });
+  };
   render() {
-    const { classes } = this.props;
+    const { classes, user } = this.props;
+    const { isFullScreen } = this.state;
 
     return (
-      <div>
-        <div className={classes.root}>App Bar</div>
-      </div>
+      <MaterialUiAppBar position="relative">
+        <Toolbar variant="dense">
+          <IconButton color="inherit">
+            <ArrowBackIcon classes={{ root: classes.backIcon }} />
+          </IconButton>
+          <IconButton color="inherit">
+            <SyncIcon />
+          </IconButton>
+          <Typography classes={{ root: classes.textMargin }} variant="h5" color="inherit">
+            PA
+          </Typography>
+          <Typography classes={{ root: classes.textMargin }} variant="h6" color="inherit">
+            {user.displayName}
+          </Typography>
+          <div className={classes.grow} />
+          <IconButton color="inherit">
+            <ViewModuleIcon />
+          </IconButton>
+          <IconButton color="inherit" onClick={this.toggleFullScreen}>
+            {isFullScreen ? <FullScreenIcon /> : <ExitFullScreenIcon />}
+          </IconButton>
+          <i className={classNames('icon-mahan-air-logo', classes.iconSize)} />
+        </Toolbar>
+      </MaterialUiAppBar>
     );
   }
 }

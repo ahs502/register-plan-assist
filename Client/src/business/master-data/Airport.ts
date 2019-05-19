ffcd89fb0979020fe3f21578fa1f0b15dd900dce
Hessam A Shokravi <ahs502@gmail.com>
2019-05-19 17:48:51 +0430

    Airport data model is updated.

--- Client/src/business/master-data/Airport.ts
+++ Client/src/business/master-data/Airport.ts
@@ -5,14 +5,14 @@ export default class Airport implements MasterDataItem {
   name: string;
 
   fullName: string;
-  isInternational: boolean;
+  international: boolean;
 
   constructor(raw: any) {
     this.id = String(raw['id']);
     this.name = String(raw['id']);
 
     this.fullName = String(raw['fullName']);
-    this.isInternational = String(raw['isInternational']) === 'true';
+    this.international = String(raw['international']) === 'true';
   }
 }
 

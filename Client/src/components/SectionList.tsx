783420ad67b8938bc9d01554f4269a30c575eb49
951168 <afshin.alizadeh@mahan.aero>
2019-05-26 16:05:43 +0430

    - Use card and ListMenu instade of div - Impliment UI/UX

--- Client/src/components/SectionList.tsx
+++ Client/src/components/SectionList.tsx
@@ -1,13 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { WithStyles, withStyles, createStyles, Theme } from '@material-ui/core/styles';
 import classNames from 'classnames';
+import { Card, CardContent, Typography, CardActionArea, MenuList, MenuItem } from '@material-ui/core';
 
 const styles = (theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
       alignItems: 'stretch',
-      border: '1px solid brown',
       margin: 0,
       padding: 0,
       height: 'calc(100vh - 104px)'
@@ -38,17 +38,24 @@ const styles = (theme: Theme) =>
     },
     card: {
       display: 'inline-block',
-      border: '1px solid black',
       margin: theme.spacing.unit * 4,
-      padding: theme.spacing.unit * 3,
-      width: theme.spacing.unit * 40,
+      // padding: theme.spacing.unit * 3,
+
       cursor: 'pointer'
+    },
+    cardContent: {
+      width: theme.spacing.unit * 40,
+      height: theme.spacing.unit * 20,
+      padding: theme.spacing.unit * 3
     },
     cardTitle: {
       fontSize: '120%'
     },
     cardDescription: {
       color: theme.palette.grey[600]
+    },
+    cardHolder: {
+      display: 'flex'
     }
   });
 
@@ -75,22 +82,29 @@ const SectionList: FunctionComponent<Props> = props => {
   return (
     <div className={classes.root}>
       <div className={classes.list}>
-        {sections.map(section => (
-          <div key={section.title} className={classNames(classes.item, { [classes.selectedItem]: section === selectedSection })}>
-            <button onClick={() => sectionSelectHandler(section)}>{section.title}</button>
-          </div>
-        ))}
+        <MenuList>
+          {sections.map(section => (
+            <MenuItem key={section.title} onClick={() => sectionSelectHandler(section)}>
+              {section.title}
+            </MenuItem>
+          ))}
+        </MenuList>
       </div>
+
       <div className={classes.contents}>
         {selectedSection ? (
           children
         ) : (
-          <div>
+          <div className={classes.cardHolder}>
             {sections.map(section => (
-              <div key={section.title} className={classes.card} onClick={() => sectionSelectHandler(section)}>
-                <div className={classes.cardTitle}>{section.title}</div>
-                <div className={classes.cardDescription}>{section.description}</div>
-              </div>
+              <Card key={section.title} className={classes.card} onClick={() => sectionSelectHandler(section)}>
+                <CardActionArea>
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant="h6">{section.title}</Typography>
+                    <Typography>{section.description}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
             ))}
           </div>
         )}

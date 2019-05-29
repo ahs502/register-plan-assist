ce37d54186e18c06b3e9069431adfc9931011cb9
951168 <afshin.alizadeh@mahan.aero>
2019-05-29 16:14:42 +0430

    - Add styles to MultiSelect

--- Client/src/components/MultiSelect.tsx
+++ Client/src/components/MultiSelect.tsx
@@ -1,8 +1,21 @@
 import React, { FunctionComponent } from 'react';
+import { WithStyles, withStyles, createStyles, Theme } from '@material-ui/core/styles';
 import MuiReactSelect, { Suggestion } from './MuiReactSelect';
 import { ValueType } from 'react-select/lib/types';
 
-export interface MultiSelectProps {
+const styles = (theme: Theme) =>
+  createStyles({
+    wrapper: {
+      display: 'flex',
+      alignItems: 'flex-end'
+    },
+    space: {
+      width: theme.spacing.unit
+    },
+    root: {}
+  });
+
+export interface MultiSelectProps extends WithStyles<typeof styles> {
   label?: string;
   placeholder?: string;
   suggestions?: ReadonlyArray<Suggestion>;
@@ -10,13 +23,13 @@ export interface MultiSelectProps {
   onChange?: (value?: ReadonlyArray<Suggestion>) => void;
 }
 
-const MultiSelect: FunctionComponent<MultiSelectProps> = ({ label, placeholder, suggestions, value, onChange }: MultiSelectProps) => {
+const MultiSelect: FunctionComponent<MultiSelectProps> = ({ label, placeholder, suggestions, value, onChange, classes }: MultiSelectProps) => {
   function handleChange(value: ValueType<Suggestion>) {
     if (typeof onChange !== 'function') return;
     onChange(value ? (value as ReadonlyArray<Suggestion>) : undefined);
   }
 
-  return <MuiReactSelect label={label} placeholder={placeholder} suggestions={suggestions} value={value} onChange={handleChange} isMulti />;
+  return <MuiReactSelect classes={{ root: classes.root }} label={label} placeholder={placeholder} suggestions={suggestions} value={value} onChange={handleChange} isMulti />;
 };
 
-export default MultiSelect;
+export default withStyles(styles)(MultiSelect);

60abdc2738464244d0376193fd9978f406e2aa5e
Hessam A Shokravi <ahs502@gmail.com>
2019-05-30 14:41:35 +0430

    Master data service API is designed.

--- Client/src/service/MasterDataService.ts
+++ Client/src/service/MasterDataService.ts
@@ -1,7 +1,45 @@
 import { apiRequest } from '../utils/api';
+import { MasterDataModel } from '../business/master-data';
+import { AircraftGroupModel } from '../business/master-data/AircraftGroup';
+import { ConstraintModel } from '../business/master-data/Constraint';
 
 const request = apiRequest.bind(null, 'MasterData');
 
 export default class MasterDataService {
-  //...
+  /**
+   * Provides all or some of master data collections.
+   */
+  static async getAll(collectionSelector?: { readonly [collectionName in keyof MasterDataModel]?: true }): Promise<Partial<MasterDataModel>> {
+    return await request('get-all', { collectionSelector });
+  }
+
+  /**
+   * Adds/edits (according to the given id in model) some aircraft group in master data and
+   * provides a partial master data model feed containing all aircraft group models.
+   */
+  static async addOrEditAircraftGroup(aircraftGroup: Readonly<AircraftGroupModel>): Promise<Readonly<Pick<MasterDataModel, 'aircraftGroups'>>> {
+    return await request('add-or-edit-aircraft-group', { aircraftGroup });
+  }
+
+  /**
+   * Removes some aircraft group in master data and provides a partial master data model feed containing all aircraft group models.
+   */
+  static async removeAircraftGroup(aircraftGroupId: string): Promise<Readonly<Pick<MasterDataModel, 'aircraftGroups'>>> {
+    return await request('remove-aircraft-group', { aircraftGroupId });
+  }
+
+  /**
+   * Adds/edits (according to the given id in model) some constraint in master data and
+   * provides a partial master data model feed containing all constraint models.
+   */
+  static async addOrEditConstraint(constraint: Readonly<ConstraintModel>): Promise<Readonly<Pick<MasterDataModel, 'constraints'>>> {
+    return await request('add-or-edit-constraint', { constraint });
+  }
+
+  /**
+   * Removes some constraint in master data and provides a partial master data model feed containing all constraint models.
+   */
+  static async removeConstraint(constraintId: string): Promise<Readonly<Pick<MasterDataModel, 'constraints'>>> {
+    return await request('remove-constraint', { constraintId });
+  }
 }
--- Server/src/service/master-data-service.ts
+++ Server/src/service/master-data-service.ts
@@ -1,6 +1,66 @@
 import { Router } from 'express';
+import asyncMiddleware from '../utils/asyncMiddleware';
+
+import { MasterDataModel } from '@business/master-data';
+import { AircraftGroupModel } from '@business/master-data/AircraftGroup';
+import { ConstraintModel } from '@business/master-data/Constraint';
 
 const router = Router();
 export default router;
 
-//...
+router.post(
+  'get-all',
+  asyncMiddleware(async data => {
+    const collectionSelector: { readonly [collectionName in keyof MasterDataModel]?: true } | undefined = data.collectionSelector;
+
+    if (!collectionSelector || collectionSelector.aircraftTypes) {
+      // do it...
+    }
+
+    return {} as Partial<MasterDataModel>;
+  })
+);
+
+router.post(
+  'add-or-edit-aircraft-group',
+  asyncMiddleware(async data => {
+    const aircraftGroup: Readonly<AircraftGroupModel> = data.aircraftGroup;
+
+    // do it...
+
+    return { aircraftGroups: [aircraftGroup] as AircraftGroupModel[] } as Pick<MasterDataModel, 'aircraftGroups'>;
+  })
+);
+
+router.post(
+  'remove-aircraft-group',
+  asyncMiddleware(async data => {
+    const aircraftGroupId: string = data.aircraftGroupId;
+
+    // do it...
+
+    return { aircraftGroups: [] as AircraftGroupModel[] } as Pick<MasterDataModel, 'aircraftGroups'>;
+  })
+);
+
+router.post(
+  'add-or-edit-constraint',
+  asyncMiddleware(async data => {
+    const constraint: Readonly<ConstraintModel> = data.constraint;
+
+    // do it...
+
+    return { constraints: [constraint] as ConstraintModel[] } as Pick<MasterDataModel, 'constraints'>;
+  })
+);
+
+router.post(
+  'remove-constraint',
+  asyncMiddleware(async data => {
+    const constraintId: string = data.constraintId;
+
+    // do it...
+
+    return { constraints: [] as ConstraintModel[] } as Pick<MasterDataModel, 'constraints'>;
+  })
+);

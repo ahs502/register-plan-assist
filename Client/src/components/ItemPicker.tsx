c94ee0d6d8c8d140195b6edba8a6eaa6925ad391
951168 <afshin.alizadeh@mahan.aero>
2019-06-19 13:45:22 +0430

    Add ItemPicker Component

---
+++ Client/src/components/ItemPicker.tsx
@@ -0,0 +1,52 @@
+import React, { useState, PropsWithChildren, ReactElement } from 'react';
+import { Theme, TextField } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  reject: {
+    color: red[500]
+  }
+}));
+
+export interface ItemPicker<T> {
+  readonly sources: ReadonlyArray<Readonly<T>>;
+  readonly fieldName: keyof T;
+  onItemSelect?: (item: T) => void;
+  defaultValue?: T;
+}
+
+const ItemPicker = <T extends {}>(props: PropsWithChildren<ItemPicker<T>>): ReactElement | null => {
+  const { sources, fieldName, onItemSelect, defaultValue } = props;
+  const selectItem = defaultValue ? defaultValue[fieldName] : '';
+  const [selectedItem, setSelectedItem] = useState<string>((selectItem as any) as string);
+  const [isValidObject, setIsValidObject] = useState(defaultValue ? sources.some(s => s[fieldName] === defaultValue[fieldName]) : false);
+
+  const classes = useStyles();
+  //const theme = useTheme();
+
+  return (
+    <TextField
+      value={selectedItem}
+      error={!isValidObject}
+      InputProps={{
+        className: !isValidObject ? classes.reject : ''
+      }}
+      onChange={e => {
+        var itemValue = e.target.value;
+        var item = sources.find(i => ((i[fieldName] as any) as string).toLowerCase() === itemValue.toLowerCase());
+
+        if (item) {
+          onItemSelect && onItemSelect(item);
+          itemValue = itemValue.toUpperCase();
+          setIsValidObject(true);
+        } else {
+          setIsValidObject(false);
+        }
+        setSelectedItem(itemValue);
+      }}
+    />
+  );
+};
+
+export default Picker;

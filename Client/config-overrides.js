69a253621fffd753c95d9829be44d6084cd98963
Hessam A Shokravi <ahs502@gmail.com>
2019-08-10 11:30:50 +0430

    Error in building client project is fixed.

--- Client/config-overrides.js
+++ Client/config-overrides.js
@@ -1,16 +1,17 @@
 const path = require('path');
-const { override, removeModuleScopePlugin, babelInclude, addWebpackAlias } = require('customize-cra');
+const { override, removeModuleScopePlugin, babelInclude, addWebpackAlias, addBabelPlugin } = require('customize-cra');
 
 module.exports = {
   webpack: override(
     removeModuleScopePlugin(),
     babelInclude([path.resolve('src'), path.resolve('../Core')]),
-    addWebpackAlias({ src: path.resolve('./src'), '@core': path.resolve('../Core') })
+    addWebpackAlias({ src: path.resolve('./src'), '@core': path.resolve('../Core') }),
+    addBabelPlugin('@babel/plugin-transform-modules-commonjs')
   ),
   devServer: function(configFunction) {
     return function(proxy, allowedHost) {
       const config = configFunction(proxy, allowedHost);
-      //TODO: See if you can implement proxy here instead of setupProxy.js file.
+      //TODO: See if you can implement proxy here instead of src/setupProxy.js file.
       return config;
     };
   }

678caff102e7d6d07024988740ac1dce20b42912
Hessam A Shokravi <ahs502@gmail.com>
2019-08-14 13:48:08 +0430

    Port 4000 for local development in OAuth server

--- Server/config.js
+++ Server/config.js
@@ -3,7 +3,7 @@ var config = {
   oauth: {
     serverUrl: 'https://login.mahan.aero/adfs/oauth2/token',
     serverIssuer: 'https://login.mahan.aero/adfs/oauth2/authorize',
-    clientUrl: 'http://localhost:4200/',
+    clientUrl: 'http://localhost:4000/',
     clientId: 'b3ca0b10-f75f-4a3b-b7f8-278b1bd6fc2a',
     resourceName: 'dev-rpa',
     lang: 'En'

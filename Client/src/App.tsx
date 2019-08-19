349f87cf8cef3e9cd6d1a597f5a2f2e00f34d06d
Hessam A Shokravi <ahs502@gmail.com>
2019-08-19 11:57:31 +0430

    Style initial loader

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,15 +1,27 @@
 import React, { FC, useState, useEffect } from 'react';
 
-import { ThemeProvider } from '@material-ui/styles';
+import { CircularProgress, Theme } from '@material-ui/core';
+import { ThemeProvider, makeStyles } from '@material-ui/styles';
 import theme from './theme';
+
+import RequestManager from './utils/RequestManager';
+import MasterDataService from './services/MasterDataService';
+import MasterData from '@core/master-data';
 
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import PreplanListPage from './pages/preplan-list';
 import PreplanPage from './pages/preplan';
 import AppBar from './components/AppBar';
-import RequestManager from './utils/RequestManager';
-import MasterDataService from './services/MasterDataService';
-import MasterData from '@core/master-data';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  progress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -24,
+    marginLeft: -24
+  }
+}));
 
 const App: FC = () => {
   const [initializing, setInitializing] = useState(true);
@@ -26,9 +38,11 @@ const App: FC = () => {
 
   RequestManager.onProcessingChanged = processing => setLoading(processing);
 
+  const classes = useStyles();
+
   return (
     <ThemeProvider theme={theme}>
-      {initializing && <div>Loading, please wait...</div>}
+      {initializing && <CircularProgress size={48} className={classes.progress} />}
       {!initializing && (
         <Router>
           <AppBar loading={loading} fullScreen={fullScreen} />

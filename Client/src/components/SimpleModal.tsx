8b6784f5fcefb8b5ae479b1ce30112724f2dffeb
951168 <afshin.alizadeh@mahan.aero>
2019-08-19 14:23:30 +0430

    Impliment Add/Edit/Delete/Publish Preplan

--- Client/src/components/SimpleModal.tsx
+++ Client/src/components/SimpleModal.tsx
@@ -1,25 +1,32 @@
 import React, { FC, useState, useEffect } from 'react';
-import { Theme, DialogTitle, DialogContent, DialogActions, Button, CircularProgress } from '@material-ui/core';
+import { Theme, DialogTitle, DialogContent, DialogActions, Button, CircularProgress, Paper, Typography } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/styles';
 import DraggableDialog, { DraggableDialogProps } from './DraggableDialog';
-import { green } from '@material-ui/core/colors';
+import classNames from 'classnames';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    display: 'inline-block',
-    border: '1px solid blue',
-    borderRadius: 5,
-    margin: theme.spacing(3),
-    padding: theme.spacing(3),
-    backgroundColor: theme.palette.extraColors.backupRegister,
-    color: theme.palette.text.primary
-  },
   progress: {
     position: 'absolute',
     top: '50%',
     left: '50%',
     marginTop: -12,
     marginLeft: -12
+  },
+  disable: {
+    opacity: 0.5
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  marginTop2: {
+    marginTop: theme.spacing(2)
+  },
+  error: {
+    padding: theme.spacing(1.5, 1.5, 1.5, 3)
+  },
+  paperError: {
+    boxShadow: '0px 1px 3px 0px rgba(255, 0, 0, 0.2), 0px 1px 1px 0px rgba(255, 0, 0, 0.14), 0px 2px 1px -1px rgba(255, 0, 0, 0.12);'
   }
 }));
 
@@ -38,15 +45,24 @@ export interface ModalAction {
   action?(): void;
 }
 
-const SimpleModal: FC<SimpleModalProps> = ({ children, loading, title, cancelable, onClose, actions: modalActions, ...other }) => {
+const SimpleModal: FC<SimpleModalProps> = ({ children, loading, title, cancelable, onClose, actions: modalActions, errorMessage, ...other }) => {
   if (cancelable && !onClose) throw 'Cancelable SimpleModals need onClose handler to be defined.';
   const classes = useStyles();
   return (
     <DraggableDialog {...other} aria-labelledby="form-dialog-title" disableBackdropClick={loading || !cancelable} disableEscapeKeyDown={loading || !cancelable} onClose={onClose}>
-      <DialogTitle id="form-dialog-title">{title}</DialogTitle>
+      <DialogTitle className={loading ? classes.disable : ''} id="form-dialog-title">
+        {title}
+      </DialogTitle>
       <DialogContent>
-        {children}
+        <div className={classNames(loading ? classes.disable : '')}>{children}</div>
         {loading && <CircularProgress size={24} className={classes.progress} />}
+        {errorMessage && (
+          <Paper className={classNames(classes.paperError, classes.marginTop2)}>
+            <Typography className={classes.error} component="p" variant="body1" color="error">
+              {errorMessage}
+            </Typography>
+          </Paper>
+        )}
       </DialogContent>
       <DialogActions>
         {modalActions.map(ma => (
@@ -55,6 +71,7 @@ const SimpleModal: FC<SimpleModalProps> = ({ children, loading, title, cancelabl
               const handler = ma.action || onClose;
               handler && handler();
             }}
+            disabled={loading}
             color="primary"
           >
             {ma.title}

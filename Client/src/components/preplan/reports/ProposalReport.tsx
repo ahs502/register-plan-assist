64908b81ca70ee4035cb705ca302d7cae981bb2b
951168 <afshin.alizadeh@mahan.aero>
2019-09-15 12:34:21 +0430

    bugfix: Set category to '' if category is null or undefined

--- Client/src/components/preplan/reports/ProposalReport.tsx
+++ Client/src/components/preplan/reports/ProposalReport.tsx
@@ -1385,7 +1385,7 @@ export default ProposalReport;
 function groupFlattenFlightRequirmentbyCategory(realFlatModel: FlattenFlightRequirment[]) {
   const groupObject = realFlatModel.reduce(
     (acc, current) => {
-      const category = current.category;
+      const category = current.category || '';
       acc[category] = acc[category] || [];
       acc[category].push(current);
       return acc;

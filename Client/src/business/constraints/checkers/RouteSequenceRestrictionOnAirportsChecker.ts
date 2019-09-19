6b7d35b0bc528ae555265b2fd6b53b6d434b2685
Hessamoddin A Shokravi <ahs502@gmail.com>
2019-09-19 12:41:39 +0430

    Bypass implementation of other checkers

--- Client/src/business/constraints/checkers/RouteSequenceRestrictionOnAirportsChecker.ts
+++ Client/src/business/constraints/checkers/RouteSequenceRestrictionOnAirportsChecker.ts
@@ -3,13 +3,17 @@ import Objection from 'src/business/constraints/Objection';
 import Preplan from 'src/business/Preplan';
 import ConstraintSystem from 'src/business/constraints/ConstraintSystem';
 import { Constraint } from '@core/master-data';
+import { RouteSequenceRestrictionOnAirportsConstraintData } from '@core/master-data/Constraint';
 
 export default class RouteSequenceRestrictionOnAirportsChecker extends Checker {
+  private data: RouteSequenceRestrictionOnAirportsConstraintData;
+
   constructor(preplan: Preplan, constraintSystem: ConstraintSystem, constraint: Constraint) {
     super(preplan, constraintSystem, constraint.template, constraint);
+    this.data = constraint.data as RouteSequenceRestrictionOnAirportsConstraintData;
   }
 
   check(): Objection[] {
-    throw 'Not implemented.';
+    return []; //TODO: Not implemented.
   }
 }
